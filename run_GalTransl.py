bc7ae2fd900b69b91a8b31443c9b513cabfe8eb8
c0re100 <corehusky@gmail.com>
2024-09-21 20:23:57 +0800

    Remove shortcut

--- run_GalTransl.py
+++ run_GalTransl.py
@@ -117,8 +117,8 @@ class ProjectManager:
                 except KeyboardInterrupt:
                     print("\nGoodbye.")
                     return
-            if self.translator not in ["showplugs", "dump-name"]:
-                self.create_shortcut_win()
+            # if self.translator not in ["showplugs", "dump-name"]:
+            #     self.create_shortcut_win()
             worker(
                 self.project_dir,
                 self.config_file_name,
